0899c51cf70766a624a6d43a327e8b9e3b63f446
Moise1 <rwibutso.mo@gmail.com>
2021-11-26 19:13:10 +0200

    explore touchables

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  TouchableHighlight,
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
-export default function App() {
+export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2}> Welcome to DoneWithIt. </Text>
+      <TouchableNativeFeedback>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "green" }}
+        />
+      </TouchableNativeFeedback>
+      <TouchableHighlight onPress={() => console.log("Image Touched.")}>
+        <Image
+          style={styles.image}
+          source={{ uri: "https://picsum.photos/id/1038/200/300" }}
+        />
+      </TouchableHighlight>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    height: 300,
+    width: 200,
   },
 });
+
+export default App;
+
+
+// Stopped at part 1 - lesson 2 - Vid 5
